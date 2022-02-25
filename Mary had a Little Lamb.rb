e2c5174ec1cec8d81b8592a6ef252d5fa0e4070c
IsaaBae <99359069+IsaaBae@users.noreply.github.com>
2022-02-25 10:33:44 -0800

    Create Mary had a Little Lamb.rb

---
+++ Mary had a Little Lamb.rb
@@ -0,0 +1,18 @@
+# Welcome to Sonic Pi
+live_loop:mary do
+  use_bpm 138
+  use_synth :kalimba
+  play :b4, amp: 23
+  sleep 1
+  play :a4, amp: 23
+  sleep 1
+  play :g4, amp: 23
+  sleep 1
+  play :a4, amp: 23
+  sleep 1
+  play :b4, amp: 23
+  sleep 1
+  play :b4, amp: 23
+  sleep 1
+  play :b4, amp: 23
+end
