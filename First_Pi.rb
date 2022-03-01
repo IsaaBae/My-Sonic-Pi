19c296445bb4af975f4f3d79b8076b9c7c4c6e4d
IsaaBae <99359069+IsaaBae@users.noreply.github.com>
2022-03-01 10:19:53 -0800

    Create First_Pi.rb

---
+++ First_Pi.rb
@@ -0,0 +1,22 @@
+live_loop:ooga_booga do
+  sample :bd_pure, beat_stretch: 1, amp: 1
+  sleep 0.5
+end
+live_loop:corn_onna_cob do
+  sample :loop_compus,beat_stretch: 4, cutoff: 130
+  sleep 4
+end
+live_loop:lucky_charms do
+  sample :bd_pure, beat_stretch: 1, amp: 1
+  sleep 0.5
+  sample :bd_pure, beat_stretch: 1, amp: 1
+  sleep 0.5
+end
+live_loop:blue_shirt do
+  sample :sn_dolf
+  sleep 1
+end
+live_loop:blue_shirt do
+  sample :vinyl_backspin
+  sleep 8
+end
